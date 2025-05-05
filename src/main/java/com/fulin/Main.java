f3df31359714f8f41d03663c39d9dbd812a92d37
SunLelin <1376757343@qq.com>
2025-05-05 13:05:12 +0800

    init:创建项目

---
+++ src/main/java/com/fulin/Main.java
@@ -0,0 +1,12 @@
+package com.fulin;
+
+/**
+ * @Author: Fulin
+ * @Description: 主函数
+ * @DateTime: 2025/5/5 下午1:02
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
